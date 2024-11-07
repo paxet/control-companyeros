6dcfa5d00ab9d04b248998cf47b307eeccdf93f5
Javier Caballero <jacallo@protonmail.com>
2024-11-07 08:28:02 +0100

    feat: notice tooltip and disclose left coworkers

--- ControlCompanyeros.user.js
+++ ControlCompanyeros.user.js
@@ -3,22 +3,44 @@
 // @name:es        Compañeros conectados
 // @namespace      Violentmonkey Scripts
 // @match          https://intranet.iti.upv.es/*/controlhorario/
-// @grant          none
+// @grant          GM.getValue
+// @grant          GM.setValue
 // @version        0.4.0
 // @author         Javier C.
 // @description    Show status for ITI coworkers.
 // @description:es Muestra el estado de los compañeros del ITI.
 // @run-at         document-idle
-// @downloadURL    https://raw.githubusercontent.com/paxet/control-companyeros/refs/heads/main/ControlCompanyeros.js
+// @inject-into    content
+// @homepageURL
+// @supportURL
+// @downloadURL    https://raw.githubusercontent.com/paxet/control-companyeros/refs/heads/main/ControlCompanyeros.user.js
 // ==/UserScript==
 
-function divUsuario(trabajador, color) {
-  return `
-		<div style="color: ${color};">
-			<img data-v-18dfe0cb="" width="15" height="auto" src="${trabajador.imagen}">
-			[${trabajador.area}] ${trabajador.nombre} (${getTimeFormat(trabajador.tiempo)})
-		</div>
-        `;
+function genUserSection(trabajador, color) {
+  let userDiv = `<div style="color: ${color};">`;
+
+  if (trabajador.estado == 'left') {
+    userDiv += `
+      <span id="today_help" class="badge badge-success pull-right help-badge" data-original-title="" title="${getTimeFormat(trabajador.tiempo)}">
+        <strong>?</strong>
+      </span>`;
+  } else {
+    if (trabajador.aviso) {
+      userDiv += `
+        <span id="today_help" class="badge badge-success pull-right help-badge" data-original-title="" title="${trabajador.aviso}">
+          <strong>?</strong>
+        </span>`;
+    }
+  }
+  userDiv += `
+			<img data-v-18dfe0cb="" width="15" height="auto" src="${trabajador.imagen}" alt="${trabajador.nombre}">
+			[${trabajador.area}] ${trabajador.nombre}`;
+
+  if (trabajador.tipo != 'per') {
+    userDiv += ` (${getTimeFormat(trabajador.tiempo)})`;
+  }
+  userDiv += `	</div>`;
+  return userDiv;
 }
 
 function crearListado(trabajadores) {
@@ -26,11 +48,13 @@ function crearListado(trabajadores) {
   let ausentes = [];
   let desconectados = [];
   let enVacaciones = [];
+  let retirados = [];
 
   trabajadores = trabajadores.sort((a, b) =>
     a.tiempo > b.tiempo ? 1 : b.tiempo > a.tiempo ? -1 : 0
   );
   trabajadores.forEach(trabajador => {
+    //TODO: Comprobar el person.status por si ya no sigue en la empresa, ejemplo: id=252
     switch (trabajador.estado) {
       case 'available':
         conectados.push(trabajador);
@@ -46,6 +70,9 @@ function crearListado(trabajadores) {
       case 'off_work':
         desconectados.push(trabajador);
         break;
+      case 'left':
+        retirados.push(trabajador);
+        break;
     }
   });
 
@@ -53,38 +80,50 @@ function crearListado(trabajadores) {
   let ausentesHTML = '';
   let desconectadosHTML = '';
   let enVacacionesHTML = '';
+  let retiradosHTML = '';
 
   if (conectados.length > 0) {
     conectadosHTML =
       '<div style="margin-top: 5px;"><strong style="color: #1c84c6">Conectados</strong></div>';
     conectados.forEach(t => {
-      conectadosHTML += divUsuario(t, '#1c84c6');
+      conectadosHTML += genUserSection(t, '#1c84c6');
     });
   }
   if (ausentes.length > 0) {
     ausentesHTML =
       '<div style="margin-top: 5px;"><strong style="color: #f8ac59">Ausentes</strong></div>';
     ausentes.forEach(t => {
-      ausentesHTML += divUsuario(t, '#f8ac59');
+      ausentesHTML += genUserSection(t, '#f8ac59');
     });
   }
   if (desconectados.length > 0) {
     desconectadosHTML =
       '<div style="margin-top: 5px;"><strong style="color: #ed5565">Desconectados</strong></div>';
     desconectados.forEach(t => {
-      desconectadosHTML += divUsuario(t, '#ed5565');
+      desconectadosHTML += genUserSection(t, '#ed5565');
     });
   }
   if (enVacaciones.length > 0) {
     enVacacionesHTML =
       '<div style="margin-top: 5px;"><strong style="color: #a7bf44">En vacaciones</strong></div>';
     enVacaciones.forEach(t => {
-      enVacacionesHTML += divUsuario(t, '#a7bf44');
+      enVacacionesHTML += genUserSection(t, '#a7bf44');
+    });
+  }
+  if (retirados.length > 0) {
+    retiradosHTML =
+      '<div style="margin-top: 5px;"><strong style="color: #444444">Contrato finalizado</strong></div>';
+    retirados.forEach(t => {
+      retiradosHTML += genUserSection(t, '#444444');
     });
   }
   const html =
-    conectadosHTML + ausentesHTML + desconectadosHTML + enVacacionesHTML;
-  document.getElementsByClassName('accordions-container')[0].innerHTML += html;
+    conectadosHTML +
+    ausentesHTML +
+    desconectadosHTML +
+    enVacacionesHTML +
+    retiradosHTML;
+  return html;
 }
 
 function getTimeFormat(t) {
@@ -103,16 +142,18 @@ function httpGet(theUrl) {
   return xmlHttp.responseText;
 }
 
-function getInfo(jsonString) {
+function dataFromPersonResponse(jsonString) {
   const obj = JSON.parse(jsonString);
+  //TODO: Falta el person.status
   return {
     nombre: `${obj.person.firstname} ${obj.person.surname}`,
     available: obj.activity.available,
-    estado: obj.activity.status,
+    estado: obj.person.status == 'inactive' ? 'left' : obj.activity.status,
     tipo: obj.activity.type,
     imagen: obj.person.image,
     tiempo: new Date(obj.activity.timestamp),
     area: obj.person.organizational_unit.abbreviation,
+    aviso: obj.activity.notice,
   };
 }
 
@@ -122,28 +163,74 @@ function getTrabajadores(ids) {
     const data = httpGet(
       `https://intranet.iti.upv.es/iti-hrm/controlhorario/informe-ultimo-fichaje/persona/${id}`
     );
-    infoTrabajadores.push(getInfo(data));
+    infoTrabajadores.push(dataFromPersonResponse(data));
   });
   return infoTrabajadores;
 }
 
-function whenElementLoaded(ids) {
-  const infoTrabajadores = getTrabajadores(ids);
-  crearListado(infoTrabajadores);
+function getElementWidget() {
+  return document.getElementById('coworkers-status');
+}
+
+function generateElementWidget() {
+  const rootElement = document.getElementsByClassName(
+    'accordions-container'
+  )[0];
+
+  //TODO: Create parent with nested divs, one for coworkers and another for button to add or clear all.
+  let csElement = document.createElement('div');
+  csElement.setAttribute('id', 'coworkers-status');
+  csElement.innerHTML = 'Cargando...';
+
+  rootElement.appendChild(csElement);
+
+  return csElement;
+}
+
+function addCoworker() {
+  let text = prompt('Indica el ID de tu compi', '9876');
+  //TODO: Ask for login and make call to obtain ID for it.
+  //TODO: Add some checks and sanitize value.
+  GM.getValue('coworkers', '').then(
+    function (value) {
+      GM.setValue('coworkers', `${value},${text}`);
+    },
+    function (error) {
+      console.log(`Error accediendo localStorage: ${error}`);
+    }
+  );
+}
+
+function loadFromText(value) {
+  const coworkers = value.split(',');
+  let csElement = getElementWidget();
+
+  if (csElement == null) {
+    csElement = generateElementWidget();
+  }
+
+  if (coworkers.length >= 1 && coworkers[0] != '') {
+    let infoTrabajadores = getTrabajadores(coworkers);
+    csElement.innerHTML = crearListado(infoTrabajadores);
+  } else {
+    csElement.innerHTML = 'Recuerda <strong>indicar</strong> los IDs.';
+  }
 }
 
 (function () {
   'use strict';
-  const idsTrabajadores = []; // Pon aquí los ids de tus compañeros
+
   var intervalID = setInterval(function () {
     if (document.getElementsByClassName('accordions-container').length) {
       clearInterval(intervalID);
-      if (idsTrabajadores.length == 0) {
-        let message = 'Recuerda <strong>indicar</strong> los IDs.';
-        document.getElementsByClassName('accordions-container')[0].innerHTML +=
-          message;
-      }
-      whenElementLoaded(idsTrabajadores);
+      GM.getValue('coworkers', '').then(
+        function (value) {
+          loadFromText(value);
+        },
+        function (error) {
+          console.log(`Se produjo un error: ${error}`);
+        }
+      );
     }
   }, 350);
 })();
